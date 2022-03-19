0b55f5a1bc3e40126e7b3fb7c342cb5714207f6b
vincenttuan <vincentjava@yahoo.com.tw>
2022-03-19 17:01:06 +0800

    psi

--- src/main/java/com/psi/repository/ProductRepository.java
+++ src/main/java/com/psi/repository/ProductRepository.java
@@ -19,4 +19,11 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 		   		"FROM products p")
 	List<Inventory> queryInventories();
 	
+	@Query(nativeQuery = true,
+			   value = "SELECT p.id, p.name, p.cost, p.price, \n" + 
+			   		"(SELECT sum(amount) FROM purchase_items WHERE product_id = p.id limit 1) as amount1, \n" + 
+			   		"(SELECT sum(amount) FROM order_items WHERE product_id = p.id limit 1) as amount2 \n" + 
+			   		"FROM products p WHERE p.id=:id")
+	Inventory findInventoryById(Long id);
+	
 }
---
+++ src/main/java/com/psi/validator/InventoryValidator.java
@@ -0,0 +1,50 @@
+package com.psi.validator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import com.psi.entity.OrderItem;
+import com.psi.entity.view.Inventory;
+import com.psi.repository.ProductRepository;
+
+@Component
+public class InventoryValidator implements Validator {
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return OrderItem.class.isAssignableFrom(clazz);
+	}
+	
+	@Override
+	public void validate(Object target, Errors errors) {
+		OrderItem orderItem = (OrderItem)target;
+		if(orderItem.getAmount() == null || orderItem.getAmount() == 0) {
+			errors.rejectValue("amount", "order_item.amount.required", "請輸入數量");
+		} else {
+			Inventory inventory = productRepository.findInventoryById(orderItem.getProduct().getId());
+			int remaining = 0;
+			try {
+				// amount1 : 進貨數量
+				// amount2 : 銷售數量
+				if(inventory.getAmount2() == null) { // 是否有無銷售數量
+					// 若無銷售數量，則庫存數量 = 進貨數量
+					remaining = inventory.getAmount1();
+				} else {
+					remaining = inventory.getAmount1() - inventory.getAmount2();
+				}
+				if(orderItem.getAmount() > remaining) {
+					errors.rejectValue("amount", "inventory.amount.insufficient", "目前庫存數量不足（庫存：" + remaining + "）");
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				errors.rejectValue("amount", "inventory.amount.error", "此商品無進貨資料無法計算庫存");
+			}
+		}
+	}
+
+}
