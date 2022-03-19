b84d1f51cf30328cc23a20eb9e8c89a347fb1fa5
vincenttuan <vincentjava@yahoo.com.tw>
2022-03-19 15:36:34 +0800

    psi

--- src/main/java/com/psi/controller/OrderController.java
+++ src/main/java/com/psi/controller/OrderController.java
@@ -1,14 +1,21 @@
 package com.psi.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.psi.entity.Customer;
 import com.psi.entity.Order;
+import com.psi.entity.Purchase;
 import com.psi.repository.CustomerRepository;
 import com.psi.repository.EmployeeRepository;
 import com.psi.repository.OrderItemRepository;
@@ -57,6 +64,45 @@ public class OrderController {
 		model.addAttribute("customers", customerRepository.findAll());
 		model.addAttribute("employees", employeeRepository.findAll());
 		return "order";
+	}
+	@GetMapping("/{id}")
+	public String get(@PathVariable("id") Long id, Model model) {
+		model.addAttribute("_method", "PUT");
+		model.addAttribute("order", orderRepository.findById(id));
+		model.addAttribute("orders", orderRepository.findAll());
+		model.addAttribute("customers", customerRepository.findAll());
+		model.addAttribute("employees", employeeRepository.findAll());
+		return "order";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable("id") Long id) {
+		orderRepository.deleteById(id);
+		return "redirect:../";
+	}
+	
+	@PostMapping("/")
+	public String add(@Valid @ModelAttribute Order order, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			model.addAttribute("_method", "POST");
+			model.addAttribute("orders", orderRepository.findAll());
+			model.addAttribute("customers", customerRepository.findAll());
+			model.addAttribute("employees", employeeRepository.findAll());
+		}
+		orderRepository.save(order);
+		return "redirect:./";
+	}
+	
+	@PutMapping("/")
+	public String update(@Valid @ModelAttribute Order order, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			model.addAttribute("_method", "PUT");
+			model.addAttribute("orders", orderRepository.findAll());
+			model.addAttribute("customers", customerRepository.findAll());
+			model.addAttribute("employees", employeeRepository.findAll());
+		}
+		orderRepository.save(order);
+		return "redirect:./";
 	}
 }
 
