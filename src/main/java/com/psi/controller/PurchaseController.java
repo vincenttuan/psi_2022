2d3df72d2c0df7bb3deeddecc7d5ac34078a98ae
vincenttuan <vincentjava@yahoo.com.tw>
2022-03-19 14:23:00 +0800

    psi

--- src/main/java/com/psi/controller/PurchaseController.java
+++ src/main/java/com/psi/controller/PurchaseController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -124,10 +125,22 @@ public class PurchaseController {
 		return "purchaseitem";
 	}
 	
+	@GetMapping("/{pid}/item/delete/{iid}")
+	public String deleteItem(@PathVariable("iid") long iid) {
+		purchaseItemRepository.deleteById(iid);
+		return "redirect:../";
+	}
+	
 	@PostMapping("/{pid}/item")
 	public String addItem(PurchaseItem purchaseItem, @PathVariable("pid") long pid)  {
 		purchaseItemRepository.save(purchaseItem);
-		return "redirect:./";
+		return "redirect:./item";
+	}
+	
+	@PutMapping("/{pid}/item")
+	public String updateItem(PurchaseItem purchaseItem, @PathVariable("pid") long pid)  {
+		purchaseItemRepository.save(purchaseItem);
+		return "redirect:./item";
 	}
 	
 }
